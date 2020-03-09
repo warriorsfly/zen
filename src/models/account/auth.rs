cc57478a134b5e533d76cd66bb42160b8f16f69c
walker <warriorsfly@gmail.com>
2020-03-09 18:50:42 +0800

    update database to enable mysql

--- src/models/account/auth.rs
+++ src/models/account/auth.rs
@@ -31,18 +31,4 @@ pub struct AuthAccount {
     pub phone: String,
 }
 
-// impl Responder for UserAuth {
-//     type Error = Error;
-//     type Future = Ready<Result<HttpResponse, Error>>;
-
-//     fn respond_to(self, _req: &HttpRequest) -> Self::Future {
-//         let body = serde_json::to_string(&self).unwrap();
-
-//         // Create response and set content type
-//         ready(Ok(HttpResponse::Ok()
-//             .content_type("application/json")
-//             .body(body)))
-//     }
-// }
-
 impl UserAuth {}
