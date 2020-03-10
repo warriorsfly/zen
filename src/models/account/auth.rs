c943b1271aa52f2e21247476bc0b51591bbddf77
walker <warriorsfly@gmail.com>
2020-03-10 10:08:48 +0800

    change models

--- src/models/account/auth.rs
+++ src/models/account/auth.rs
@@ -1,21 +1,16 @@
 use crate::database::PoolType;
 use crate::errors::ServiceError;
-use actix_web::{Error, HttpRequest, HttpResponse, Responder};
+
 use chrono;
 use diesel::prelude::*;
 use serde::Serialize;
 use uuid::Uuid;
 
-// use crate::{
-//     config::db::Connection,
-//     constants,
-//     entity::user::token::UserToken,
-//     schema::user_auth::{self, dsl::*},
-// };
+use crate::schema::user_auth::{self, dsl::*};
 
 /// 登录信息表
-#[derive(Debug, Serialize, Deserialize, Queryable)]
-// #[table_name = "user_auth"]
+#[derive(Debug, Serialize, Deserialize, Queryable, Identifiable, Insertable)]
+#[table_name = "user_auth"]
 pub struct UserAuth {
     pub id: String,
     pub uid: String,
@@ -27,14 +22,26 @@ pub struct UserAuth {
     pub updated_at: chrono::NaiveDateTime,
 }
 
-#[derive(Debug, Serialize, Deserialize, Queryable)]
+/// remove password,remove created_at,updated_at
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct AuthResponse {
-    pub id: String,
+    pub id: Uuid,
     pub uid: String,
     pub identity_type: i32,
     pub identifier: String,
-    pub certificate: String,
     pub login_session: String,
+}
+
+impl From<UserAuth> for AuthResponse {
+    fn from(user: UserAuth) -> Self {
+        AuthResponse {
+            id: Uuid::parse_str(&user.id).unwrap(),
+            uid: user.uid,
+            identity_type: user.identity_type,
+            identifier: user.identifier,
+            login_session: user.login_session,
+        }
+    }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -44,38 +51,32 @@ pub struct AuthAccount {
     pub phone: String,
 }
 
-impl UserAuth {
-    /// find a userauth by the user's id or error out
-    pub fn find(pool: &PoolType, uid: Uuid) -> Result<AuthResponse, ServiceError> {
-        use crate::schema::user_auth::dsl::{id, user_auth};
+/// find a userauth by the user's id or error out
+pub fn find_by_id(pool: &PoolType, user_id: Uuid) -> Result<AuthResponse, ServiceError> {
+    let not_found = format!("User {} not found", user_id);
+    let conn = pool.get()?;
+    let auth = user_auth
+        .find(user_id.to_string())
+        .first::<UserAuth>(&conn)
+        .map_err(|_| ServiceError::NotFound(not_found))?;
 
-        let not_found = format!("User {} not found", uid);
-        let conn = pool.get()?;
-        let auth = user_auth
-            .filter(id.eq(uid.to_string()))
-            .first::<AuthResponse>(&conn)
-            .map_err(|_| ServiceError::NotFound(not_found))?;
+    Ok(auth.into())
+}
 
-        Ok(auth.into())
-    }
+/// Find a user-auth by the user's authentication information
+pub fn find_by_auth(
+    pool: &PoolType,
+    phone: &str,
+    password: &str,
+    account_type: i32,
+) -> Result<AuthResponse, ServiceError> {
+    let conn = pool.get()?;
+    let auth = user_auth
+        .filter(identifier.eq(phone.to_string()))
+        .filter(certificate.eq(password.to_string()))
+        .filter(identity_type.eq(account_type))
+        .first::<UserAuth>(&conn)
+        .map_err(|_| ServiceError::Unauthorized("Invalid login".into()))?;
 
-    /// Find a user-auth by the user's authentication information
-    pub fn find(
-        pool: &PoolType,
-        phone: &str,
-        password: &str,
-        account_type: &str,
-    ) -> Result<AuthResponse, ServiceError> {
-        use crate::schema::user_auth::dsl::{certificate, identifier, identity_type, user_auth};
-
-        let conn = pool.get()?;
-        let auth = user_auth
-            .filter(identifier.eq(phone.to_string()))
-            .filter(certificate.eq(password.to_string()))
-            .filter(identity_type.eq(account_type))
-            .first::<AuthResponse>(&conn)
-            .map_err(|_| ServiceError::Unauthorized("Invalid login".into()))?;
-
-        Ok(auth.into())
-    }
+    Ok(auth.into())
 }
