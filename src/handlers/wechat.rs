bc0897891171e25b6281f39163da782dbd696505
walker <warriorsfly@gmail.com>
2020-03-13 14:11:30 +0800

    awc for wechat mini program

--- src/handlers/wechat.rs
+++ src/handlers/wechat.rs
@@ -1,8 +1,21 @@
-use crate::config::Config;
-use actix_web::client::Client;
+use crate::config::CONFIG;
+use actix_web::{client::Client, web::Path};
 
-// pub async fn wx_login() {
-//     let mut client = Client::default();
-//     Config
-//     let client.get("https://api.weixin.qq.com/sns/jscode2session").
+pub struct WxLoginResponse {
+    pub openid: String,
+    pub session_key: String,
+    pub unionid: String,
+    pub errcode: i32,
+    pub errmsg: String,
+}
+
+// pub async fn wx_login(jscode: Path<String>) {
+//     let client = Client::default();
+//     let config = &CONFIG;
+//     let url =format!("https://api.weixin.qq.com/sns/jscode2session?appid={appid}&secret={secret}&js_code={code}&grant_type=authorization_code",appid=config.wechat_appid,secret=config.wechat_secret,code=jscode);
+//     let payload = client.get(url).send().await.unwrap().body().await.unwrap();
+
+//     match payload {
+//         Payload(res) if res
+//     }
 // }
