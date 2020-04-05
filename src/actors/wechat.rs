fe2f0c21f75dd61609cbc6c405f1997d1c1812e5
walker <warriorsfly@gmail.com>
2020-04-05 23:17:07 +0800

    remove casbin for now

--- src/actors/wechat.rs
+++ src/actors/wechat.rs
@@ -33,7 +33,7 @@ impl Broadcaster {
 
     fn spawn_ping_wx(me: Data<Mutex<Self>>) {
         actix_rt::spawn(async move {
-            me.lock().unwrap().ping_wx().await;
+            let res = me.lock().unwrap().ping_wx().await;
             let mut task = interval_at(Instant::now(), Duration::from_secs(7140));
             loop {
                 task.tick().await;
