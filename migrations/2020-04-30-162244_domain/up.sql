bf2d101941745e4a0b8ca750d11f8cf9c270e04b
walker <warriorsfly@gmail.com>
2020-05-01 14:57:06 +0800

    add new model

---
+++ migrations/2020-04-30-162244_domain/up.sql
@@ -0,0 +1,9 @@
+-- 域名
+CREATE TABLE domain
+(
+  id VARCHAR(36) NOT NULL PRIMARY KEY,
+  shop_id  VARCHAR(36) NOT NULL DEFAULT '',-- COMMENT '商铺id',
+  host VARCHAR(64) NOT NULL DEFAULT '',-- COMMENT '服务器明成',
+  ssl_enabled bool  NOT NULL DEFAULT false,---- COMMENT '是否启用ssl',
+  url bool  NOT NULL DEFAULT '',---- COMMENT 'url'
+)
