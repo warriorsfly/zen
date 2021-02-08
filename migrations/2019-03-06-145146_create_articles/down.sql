0e9b3d02406cc941a6a75f18c4aeab7eb7f11d22
walker <warriorsfly@gmail.com>
2021-02-08 23:42:35 +0800

    to microservice

--- migrations/2019-03-06-145146_create_articles/down.sql
+++
@@ -1,3 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP TABLE favorite_articles;
-DROP TABLE articles;
--- src/models/mod.rs
+++
@@ -1,6 +0,0 @@
-mod article;
-mod comment;
-mod follower;
-mod user;
-
-pub use self::{article::*, comment::*, follower::*, user::*};
